6687d793086faf632582faf18013ee6ec0ff7b1f
Malithi <malithipabodha08@gmail.com>
2026-03-18 13:39:07 +0530

    Created Ticket Entity

---
+++ backend/src/main/java/com/sliit/smartcampus/entity/Ticket.java
@@ -0,0 +1,67 @@
+package com.sliit.smartcampus.entity;
+
+import com.sliit.smartcampus.enumtypes.TicketPriority;
+import com.sliit.smartcampus.enumtypes.TicketStatus;
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Table(name = "tickets")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Ticket {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    // Student inputs
+    @Column(nullable = false)
+    private String title;
+
+    @Column(length = 2000, nullable = false)
+    private String description;
+
+    @Column(nullable = false)
+    private String category;
+
+    @Column(nullable = false)
+    private String location;
+
+    @Column(nullable = false)
+    private String contact;
+
+    @Column(nullable = false)
+    private String resourceName;
+
+    @Column(nullable = false)
+    private Long resourceId;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private TicketPriority priority;
+
+    // Workflow
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private TicketStatus status;
+
+    // Temporary student tracking (until login added)
+    private Long studentId;
+
+    // Admin assignment
+    private Long technicianId;
+
+    private String technicianName;
+
+    // Resolution
+    @Column(length = 2000)
+    private String resolutionNote;
+
+    // Rejected reason
+    @Column(length = 2000)
+    private String rejectionReason;
+}
